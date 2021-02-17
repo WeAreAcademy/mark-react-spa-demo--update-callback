07373dc6f2c24836b298b391ef47f96080702422
Richard Ng <richardcrng@gmail.com>
2021-02-17 11:28:26 +0000

    Add counter task

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@
  * Key takeaway: setState can take a callback function
  *  to rerender with a new state value based on a transformation
  *  of the previous state
+ *
+ * TASK: build a counter
  */
 
 import { useState } from "react";
 
 function App() {
   const [morseString, setMorseString] = useState("");
+  const [count, setCount] = useState(1);
 
   const handleAddDot = () => {
     setMorseString((prevValue) => `${prevValue}.`);
@@ -23,6 +26,11 @@ function App() {
       <p>Message we're building: {morseString}</p>
       <button onClick={handleAddDot}>Dot</button>
       <button onClick={handleAddDash}>Dash</button>
+      <hr />
+      {/* TODO: make buttons keep a count up */}
+      <p>Current count: {count}</p>
+      <button>+ 1</button>
+      <button>+ 5</button>
     </div>
   );
 }
